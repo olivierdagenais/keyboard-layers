31a28dc529d4929c41648395df091c8248d9cabc
Olivier Dagenais <olivier.dagenais@gmail.com>
2021-01-22 21:23:21 -0500

    feat: skeleton of sketch

---
+++ KeyborgAlpha/KeyborgAlpha.ino
@@ -0,0 +1,16 @@
+// Arduino sketch to enhance an ergonomic keyboard.
+// Copyright (C) 2021  Olivier Dagenais. All rights reserved.
+// Licensed under the GNU General Public License. See LICENSE in the project root.
+
+#include "Mouse.h"
+
+void setup() {
+    // TODO: initialize hardware
+    Serial.begin(115200);
+    Mouse.begin();
+}
+
+void loop() {
+    // TODO: scan inputs and translate them to outputs
+    delay(1);
+}
