a35764d202a0f86e141fb2db9353bb58371abf57
Olivier Dagenais <olivier.dagenais@gmail.com>
2021-01-26 21:11:41 -0500

    feat: add Button class & use it
    The use is somewhat trivial (toggle a flag on any press) but it makes
    development easier for the moment.

--- KeyborgAlpha/KeyborgAlpha.ino
+++ KeyborgAlpha/KeyborgAlpha.ino
@@ -5,13 +5,71 @@
 
 #include "Mouse.h"
 
+typedef void (*callback_no_params)(void);
+
+class Button {
+  protected:
+    const uint8_t _buttonPin;
+    const callback_no_params _pressed;
+    const callback_no_params _released;
+
+    volatile bool _currentButtonState;
+
+    virtual bool isPressed() {
+        int v = digitalRead(_buttonPin);
+        return (v == 0);
+    }
+
+  public:
+    Button(uint8_t buttonPin,
+           callback_no_params pressed,
+           callback_no_params released)
+        : _buttonPin(buttonPin), _pressed(pressed), _released(released) {
+        _currentButtonState = false;
+        pinMode(_buttonPin, INPUT_PULLUP);
+    }
+
+    void scan() {
+        bool newState = isPressed();
+        if (_currentButtonState) { // button was previously pressed
+            if (!newState) {       // now it's released
+                _currentButtonState = false;
+                if (_pressed != NULL) {
+                    _pressed();
+                }
+            }
+        }
+        else {              // button wasn't pressed
+            if (newState) { // now it's pressed
+                _currentButtonState = true;
+                if (_released != NULL) {
+                    _released();
+                }
+            }
+        }
+    }
+};
+
+const int NUM_BUTTONS = 5;
+Button *_buttons[NUM_BUTTONS];
+
+bool _mouseActive = false;
+void toggleMouseActive() { _mouseActive = !_mouseActive; }
+
 void setup() {
-    // TODO: initialize hardware
+    _buttons[0] = new Button(2, &toggleMouseActive, NULL);
+    _buttons[1] = new Button(3, &toggleMouseActive, NULL);
+    _buttons[2] = new Button(4, &toggleMouseActive, NULL);
+    _buttons[3] = new Button(5, &toggleMouseActive, NULL);
+    _buttons[4] = new Button(6, &toggleMouseActive, NULL);
     Serial.begin(115200);
     Mouse.begin();
 }
 
 void loop() {
-    // TODO: scan inputs and translate them to outputs
-    delay(1);
+    for (uint8_t b = 0; b < NUM_BUTTONS; b++) {
+        _buttons[b]->scan();
+    }
+
+    delay(5);
 }
