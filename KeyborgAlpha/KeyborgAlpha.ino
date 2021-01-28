a4062457ede84c508a8734052b75989d365db741
Olivier Dagenais <olivier.dagenais@gmail.com>
2021-01-27 22:09:32 -0500

    feat: add throttle support & swap inputs
    I may need to add acceleration because the regular joystick wasn't
    precise enough for small movements and the thumb slide joystick is
    sort-of precise at low speeds but then takes forever to go large
    distances.

--- KeyborgAlpha/KeyborgAlpha.ino
+++ KeyborgAlpha/KeyborgAlpha.ino
@@ -58,24 +58,29 @@ class AnalogAxis {
     const uint16_t _range;
     const uint16_t _threshold;
     const uint16_t _center;
-    // TODO: add throttle
+    const uint8_t _throttle;
 
   public:
     AnalogAxis(uint8_t analogPin,
                int16_t minAnalogValue,
                int16_t maxAnalogValue,
                uint16_t range,
-               uint16_t threshold)
+               uint16_t threshold,
+               uint8_t throttle)
         : _analogPin(analogPin),
           _minAnalogValue(minAnalogValue),
           _maxAnalogValue(maxAnalogValue),
           _range(range),
           _threshold(threshold),
-          _center(range / 2) {
+          _center(range / 2),
+          _throttle(throttle) {
         // Do I need to initialize the pin?
     }
 
-    int16_t read() {
+    int16_t read(uint8_t percent) {
+        if (0 != (percent % _throttle)) {
+            return 0;
+        }
         int16_t reading = analogRead(_analogPin);
         int16_t adjusted =
             map(reading, _minAnalogValue, _maxAnalogValue, 0, _range);
@@ -90,7 +95,7 @@ class AnalogAxis {
 const int NUM_BUTTONS = 5;
 Button *_buttons[NUM_BUTTONS];
 
-const int NUM_ANALOG_AXIS = 3;
+const int NUM_ANALOG_AXIS = 4;
 AnalogAxis *_analogAxis[NUM_ANALOG_AXIS];
 
 bool _mouseActive = false;
@@ -103,26 +108,31 @@ void setup() {
     _buttons[3] = new Button(5, &toggleMouseActive, NULL);
     _buttons[4] = new Button(6, &toggleMouseActive, NULL);
 
-    _analogAxis[0] = new AnalogAxis(0, 1023, 0, 10, 2);
-    _analogAxis[1] = new AnalogAxis(1, 1023, 0, 10, 2);
+    _analogAxis[0] = new AnalogAxis(0, 1023, 0, 10, 2, 50);
+    _analogAxis[1] = new AnalogAxis(1, 1023, 0, 10, 2, 50);
     // https://www.sparkfun.com/products/9426 (Thumb Slide Joystick) says:
     // "(...)you can expect a range of about 128 to 775 on each axis."
-    _analogAxis[2] = new AnalogAxis(2, 128, 775, 12, 3);
+    _analogAxis[2] = new AnalogAxis(2, 128, 775, 12, 3, 16);
+    _analogAxis[3] = new AnalogAxis(3, 128, 775, 12, 3, 16);
     Serial.begin(115200);
     Mouse.begin();
 }
 
+uint8_t _percent = 0;
 void loop() {
     for (uint8_t b = 0; b < NUM_BUTTONS; b++) {
         _buttons[b]->scan();
     }
 
-    int16_t x = _analogAxis[0]->read();
-    int16_t y = _analogAxis[1]->read();
-    int16_t z = _analogAxis[2]->read();
+    int16_t w = _analogAxis[0]->read(_percent);
+    int16_t z = _analogAxis[1]->read(_percent);
+    int16_t x = _analogAxis[3]->read(_percent);
+    int16_t y = _analogAxis[2]->read(_percent);
     int16_t wheel = (z == 0 ? 0 : (z > 0 ? -1 : 1));
     if (_mouseActive) {
         Mouse.move(x, y, wheel);
     }
-    delay(5);
+
+    _percent = (_percent + 1) % 100;
+    delay(1);
 }
